e448732ec6ef72f7466c25632ee6e9c778dfca2f
shubhika10 <shubhikarai21@gmail.com>
2020-10-01 20:57:31 -0400

    added the bird trajectory

--- Bird.js
+++ Bird.js
@@ -2,12 +2,25 @@ class Bird extends BaseClass {
   constructor(x,y){
     super(x,y,50,50);
     this.image = loadImage("sprites/bird.png");
+    this.smokeimage = loadImage("sprites/smoke.png");
+    this.trajectory=[];
   }
 
   display() {
-    //this.body.position.x = mouseX;
-    //this.body.position.y = mouseY;
 
     super.display();
+
+    var posX = this.body.position.x;
+    var posY = this.body.position.y;
+
+    if(this.body.velocity.x > 10 && posX > 200){
+      var position = [posX,posY];
+      this.trajectory.push(position);
+    }
+
+    for(var i = 0; i < this.trajectory.length; i++){
+      image(this.smokeimage,this.trajectory[i][0],this.trajectory[i][1]);
+    }
+    
   }
-}
+} 
